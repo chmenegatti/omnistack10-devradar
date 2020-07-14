e0da8ade7cede06f1fdc16e7fb32d14cd15793dc
Cesar Menegatti <cesarmenegatti@Cesars-MacBook-Pro.local>
2020-07-14 16:07:43 -0300

    Dev Radar Done

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true,
+	useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(process.env.PORT || 3333);
